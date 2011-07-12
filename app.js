1e94c4cb5e59cadee452bf4412fcead66d3ae01a
Aditya <aditya@netroy.in>
2011-07-12 22:18:54 +0530

    Added backlog persistance .. using iriscouch for backup .. uses worker for background

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var express = require('express'),
          io = require("socket.io"),
         irc = require("irc"),
       debug = false,
-        app = module.exports = express.createServer();
-
-
+        app = module.exports = express.createServer(),
+     Worker = require("worker").Worker,
+     worker = new Worker("worker.js");
 
 // Configuration
 app.configure(function(){
@@ -61,17 +61,40 @@ if (!module.parent) {
   console.info("Started on port %d", app.address().port);
 }
 
-
 // Bind Socket.IO server to the http server
 var io = io.listen(app);
-io.set('log level', debug?3:1);
+io.set('log level', debug?2:1);
 io.sockets.on('connection', function(client){
-  // Am i supposed to do something here ... ?? not sure yet
   // Send the Back-log
   io.sockets.emit('names', names);
   io.sockets.emit('topic', topic);
   io.sockets.emit('message', messages);
 });
+
+// Attach a listener to the worker for response
+worker.addListener("message", function (message) {
+  if(message.saved){
+    lastTimeStamp = last.time;
+    console.info("Saved the backlog at " + new Date(lastTimeStamp));
+  }else if(message.fetched && message.fetched instanceof Array && message.fetched.length > 0){
+    messages = message.fetched;
+    console.log("Fetched the backlog. Message count : " + messages.length);
+  }else{
+    console.log(message);
+  }
+});
+
+// Before initializing IRC client, trigger the worker to fetch backlog
+worker.postMessage({"fetch":true});
+
+// And set a timer to take backups every 60 seconds
+var lastTimeStamp = 0, last;
+setInterval(function(){
+  if(messages.length === 0) return;
+  last = messages[messages.length - 1];
+  if(last.time <= lastTimeStamp) return;
+  worker.postMessage({"save":messages});
+},60*1000);
 
 // Init the IRC client & connect to #hasgeek
 var ircClient = new irc.Client(server, nick, {
@@ -85,6 +108,7 @@ var ircClient = new irc.Client(server, nick, {
 
 console.info("Connecting to freenode");
 
+// Add message handlers to the IRC client
 ircClient.addListener('names', function(ch, nicks){
   if(ch.toLowerCase() !== channel) return;
   names = nicks;
@@ -100,14 +124,19 @@ ircClient.addListener('join', function(ch, nick){
   names[nick] = '';
   io.sockets.emit('join', nick);
 });
-ircClient.addListener('part', function(channel, nick){
+ircClient.addListener('part', function(ch, nick){
   if(ch.toLowerCase() !== channel) return;
   delete names[nick];
   io.sockets.emit('part', nick);
 });
+ircClient.addListener('nick', function(old, nick){
+  delete names[old];  
+  names[nick] = '';
+  io.sockets.emit('nick', old, nick);
+});
 ircClient.addListener('message', function (from, ch, text) {
   if(ch.toLowerCase() !== channel) return;
-  var packet = {from: from, channel: channel, text: text, time: (new Date()).toUTCString()};
+  var packet = {from: from, text: text, time: (new Date()).getTime()};
   messages.push(packet);
   if(messages.length > MAX_LOG) messages.shift();
   io.sockets.emit('message', packet);
