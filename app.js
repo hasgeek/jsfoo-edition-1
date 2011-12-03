75756f0c0c06f0a5085dbb51f7cef6285823238b
Aditya <aditya@netroy.in>
2011-12-03 17:57:59 +0530

    moved IRC code to a separate module

--- app.js
+++ app.js
@@ -3,66 +3,52 @@
  */
 
 var express = require('express'),
-         io = require("socket.io"),
-        irc = require("irc"),
      stylus = require("stylus"),
-        nib = require("nib"),
-       gzip = require('connect-gzip'),
       debug = false,
-        app = module.exports = express.createServer(),
-CouchClient = require('couch-client'),
- connection = CouchClient("http://jsfoobot:foobotpass@netroy.iriscouch.com/irc_jsfoo"),
-      docId = "backlog";
+        app = module.exports = express.createServer();
 
 // Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.enable('view cache');
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(stylus.middleware({
     src: __dirname + '/src',
     dest: __dirname + '/static',
     compile: function (str, path, fn) {
-      return stylus(str).set('filename', path).set('compress', true).use(nib());
+      return stylus(str).set('filename', path).set('compress', true);//.use(require("nib")());
     }
   }));
   app.use(app.router);
-  //app.use(express.static(__dirname + '/static'));
-  app.use(gzip.staticGzip(__dirname + '/static', { maxAge: 86400*365 }));
-});
-app.configure('development', function(){
-  debug = true;
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
 });
 
-var server  = "irc.freenode.net",
-   channel  = debug?"#jsfootest":"#hasgeek";
+app.configure('development', function(){
+  debug = true;
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.static(__dirname + '/static'));
+});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+  app.enable('view cache');
+  app.use(require('connect-gzip').staticGzip(__dirname + '/static', { maxAge: 86400*365 }));
+});
 
 function capitalize(str){
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 // Routes
-app.get('/irc', function(req, resp){
-  resp.render('irc', {
-    'title': 'jsFoo 2011',
-    'channel': channel,
-    'server': server
-  });
-});
 var routeRegEx = /^\/201[12]\-(bangalore|pune|chennai)\/(about\-(event|hasgeek)|schedule|venue|hacknight|videos|sponsors|credits|register)?\/?$/;
 app.get(routeRegEx, function(req, resp){
   var url = req.url;
   var params = url.match(/(2011|2012)\-(bangalore|pune|chennai)/);
+  var year = params[1];
+  var city = params[2];
   var opts = {
-    title: ['jsFoo', params[1], capitalize(params[2])].join(' ')
+    title: ['jsFoo', capitalize(city), year].join(' '),
+    path: [city, year].join('/')
   };
-  resp.render('main-' + params[0], opts);
+  resp.render('main', opts);
 });
 
 // Catch all route
@@ -70,126 +56,13 @@ app.use(function(eq, resp){
   resp.redirect("/2011-pune/");
 });
 
-// prevent server from starting as module - can be used with something like multinode
+// prevent server from starting as module - can be used with something like multinode/cluster
 if (!module.parent) {
   app.listen(process.env.app_port || 11728);
   console.info("Started on port %d", app.address().port);
 }
 
-if(debug){
-  return;
+if(!debug){
+ require('./ircClient').init(app, debug);
 }
 
-var nick  = debug?"blahblahblah":"jsFooBot",
-   names  = {},
-   topic  = "",
-messages  = [],
- MAX_LOG  = 250;
-
-// Bind Socket.IO server to the http server
-var io = io.listen(app);
-io.configure('production', function(){
-  io.set('log level', 1);
-  io.enable('browser client minification');
-  io.enable('browser client etag');
-  io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket']);
-});
-io.configure('development', function(){
-  io.set('log level', 2);
-  io.set('transports', ['websocket', 'xhr-polling']);
-});
-
-io.sockets.on('connection', function(client){
-  // Send the Back-log
-  io.sockets.emit('names', names);
-  io.sockets.emit('topic', topic);
-  io.sockets.emit('message', messages);
-});
-
-// Before initializing IRC client, pull the json from couchDB
-console.info("fetching back log");
-connection.get(docId, function(err, doc){
-  if(err){
-    console.error(err);
-    return;
-  }else if(doc.messages && doc.messages.length){
-    messages = doc.messages;
-    console.log("Fetched the backlog. Message count : " + messages.length);
-  }
-});
-
-// And set a timer to take backups every 60 seconds
-var lastTimeStamp = 0, last;
-if(!debug) setInterval(function(){
-  if(messages.length === 0) return;
-  last = messages[messages.length - 1];
-  if(last.time <= lastTimeStamp) return;
-  try{
-    connection.save({
-      "_id": docId,
-      "messages": messages
-    }, function(err, doc){
-      if(err){
-        console.error("Saving failed");
-        console.error(err);
-        return;
-      }
-      lastTimeStamp = last.time;
-      console.info("Saved the backlog at " + new Date(lastTimeStamp));
-    });
-  }catch(e){}
-},60*1000);
-
-// And on SIGHUP flush the backlog
-process.on('SIGHUP', function () {
-  messages = [];
-  lastTimeStamp = (new Date()).getTime();
-  console.log("SIGHUP recieved.. flushing IRC message backlog");
-});
-
-
-// Init the IRC client & connect to #hasgeek
-var ircClient = new irc.Client(server, nick, {
-  'channels'  : [channel],
-  'userName'  : "jsFooBot",
-  'realName'  : "jsFooBot",
-  'password'  : null,
-  'autoRejoin': true,
-  'debug'     : debug
-});
-
-console.info("Connecting to freenode");
-
-// Add message handlers to the IRC client
-ircClient.addListener('names', function(ch, nicks){
-  if(ch.toLowerCase() !== channel) return;
-  names = nicks;
-  io.sockets.emit('names', nicks);
-});
-ircClient.addListener('topic', function(ch, tp){
-  if(ch.toLowerCase() !== channel) return;
-  topic = tp;
-  io.sockets.emit('topic', topic);
-});
-ircClient.addListener('join', function(ch, nick){
-  if(ch.toLowerCase() !== channel) return;
-  names[nick] = '';
-  io.sockets.emit('join', nick);
-});
-ircClient.addListener('part', function(ch, nick){
-  if(ch.toLowerCase() !== channel) return;
-  delete names[nick];
-  io.sockets.emit('part', nick);
-});
-ircClient.addListener('nick', function(old, nick){
-  delete names[old];  
-  names[nick] = '';
-  io.sockets.emit('nick', old, nick);
-});
-ircClient.addListener('message', function (from, ch, text) {
-  if(ch.toLowerCase() !== channel) return;
-  var packet = {from: from, text: text, time: (new Date()).getTime()};
-  messages.push(packet);
-  if(messages.length > MAX_LOG) messages.shift();
-  io.sockets.emit('message', packet);
-});
---
+++ ircClient.js
@@ -0,0 +1,147 @@
+var server = "irc.freenode.net",
+    debug = false, io,
+    channel, nick, names = {}, topic = "", messages = [],
+    MAX_LOG = 400;
+   
+exports.init = function(app, _debug) {
+  debug = _debug;
+  channel = debug ? "#jsfootest" : "#hasgeek";
+  nick = debug ? "blahblahblah" : "jsFooBot";
+
+  app.get('/irc', function(req, resp){
+    resp.render('irc', {
+      'title': 'jsFoo 2011',
+      'channel': channel,
+      'server': server
+    });
+  });
+
+  initSocketIO(app);
+
+  initBacklogger();
+
+  initIRCClient();
+};
+
+function initSocketIO(app) {
+  // Bind Socket.IO server to the http server
+  io = require("socket.io").listen(app);
+
+  
+  io.configure('production', function(){
+    io.set('log level', 1);
+    io.enable('browser client minification');
+    io.enable('browser client etag');
+    io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile', 'flashsocket']);
+  });
+
+  io.configure('development', function(){
+    io.set('log level', 2);
+    io.set('transports', ['websocket', 'xhr-polling']);
+  });
+
+  io.sockets.on('connection', function(client){
+    // Send the Back-log
+    io.sockets.emit('names', names);
+    io.sockets.emit('topic', topic);
+    io.sockets.emit('message', messages);
+  });
+}
+
+function initBacklogger (){
+  var connection = require('couch-client')("http://jsfoobot:foobotpass@netroy.iriscouch.com/irc_jsfoo"),
+      docId = "backlog";
+
+  // Before initializing IRC client, pull the json from couchDB
+  console.info("fetching back log");
+  connection.get(docId, function(err, doc){
+    if(err){
+      console.error(err);
+      return;
+    }else if(doc.messages && doc.messages.length){
+      messages = doc.messages;
+      console.log("Fetched the backlog. Message count : " + messages.length);
+    }
+  });
+
+  // And set a timer to take backups every 60 seconds
+  var lastTimeStamp = 0, last;
+  if(!debug) setInterval(function(){
+    if(messages.length === 0) return;
+    last = messages[messages.length - 1];
+    if(last.time <= lastTimeStamp) return;
+    try{
+      connection.save({
+        "_id": docId,
+        "messages": messages
+      }, function(err, doc){
+        if(err){
+          console.error("Saving failed");
+          console.error(err);
+          return;
+        }
+        lastTimeStamp = last.time;
+        console.info("Saved the backlog at " + new Date(lastTimeStamp));
+      });
+    }catch(e){}
+  },60*1000);
+
+  // And on SIGHUP flush the backlog
+  process.on('SIGHUP', function () {
+    messages = [];
+    lastTimeStamp = (new Date()).getTime();
+    console.log("SIGHUP recieved.. flushing IRC message backlog");
+  });
+
+}
+
+function initIRCClient() {
+
+  // Init the IRC client & connect to #hasgeek
+  var ircClient = new (require("irc")).Client(server, nick, {
+    'channels'  : [channel],
+    'userName'  : nick,
+    'realName'  : nick,
+    'password'  : null,
+    'autoRejoin': true,
+    'debug'     : debug
+  });
+
+  console.info("Connecting to freenode");
+
+  // Add message handlers to the IRC client
+  ircClient.addListener('names', function(ch, nicks){
+    if(ch.toLowerCase() !== channel) return;
+    names = nicks;
+    io.sockets.emit('names', nicks);
+  });
+  ircClient.addListener('topic', function(ch, tp){
+    if(ch.toLowerCase() !== channel) return;
+    topic = tp;
+    io.sockets.emit('topic', topic);
+  });
+  ircClient.addListener('join', function(ch, nick){
+    if(ch.toLowerCase() !== channel) return;
+    names[nick] = '';
+    io.sockets.emit('join', nick);
+  });
+  ircClient.addListener('part', function(ch, nick){
+    if(ch.toLowerCase() !== channel) return;
+    delete names[nick];
+    io.sockets.emit('part', nick);
+  });
+  ircClient.addListener('nick', function(old, nick){
+    delete names[old];  
+    names[nick] = '';
+    io.sockets.emit('nick', old, nick);
+  });
+  ircClient.addListener('message', function (from, ch, text) {
+    if(ch.toLowerCase() !== channel) return;
+    var packet = {from: from, text: text, time: (new Date()).getTime()};
+    messages.push(packet);
+    if(messages.length > MAX_LOG) messages.shift();
+    io.sockets.emit('message', packet);
+  });
+
+}
+
