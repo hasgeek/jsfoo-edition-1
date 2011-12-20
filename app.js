24ebf893b4f4565055506a0a2cb74125478db4b7
Aditya <aditya@netroy.in>
2011-12-20 21:26:39 +0530

    removed the IRC client.. will move into a separate app... eventually

--- app.js
+++ app.js
@@ -4,9 +4,8 @@
 
 var express = require('express'),
      stylus = require("stylus"),
-      debug = false,
         app = module.exports = express.createServer(),
- routeRegEx = /^\/201[12]\-(bangalore|pune|chennai)\/(about\-(event|hasgeek)|schedule|venue|hacknight|videos|sponsors|credits|register)?\/?$/;
+ routeRegEx = /^\/(bangalore|pune|chennai)\-201[12]\/(about\-(event|hasgeek)|schedule|venue|hacknight|videos|sponsors|credits|register)?\/?$/;
 
 // Configuration
 app.configure(function(){
@@ -17,27 +16,23 @@ app.configure(function(){
     src: __dirname + '/src',
     dest: __dirname + '/static',
     compile: function (str, path, fn) {
-      return stylus(str).set('filename', path).set('compress', true);//.use(require("nib")());
+      return stylus(str).set('filename', path).set('compress', true);
+      //.use(require("nib")());
     }
   }));
   app.use(app.router);
 
-  // Use gzippo to compress all textual content
+  // Use gzippo to compress all text content
   var gzippo = require('gzippo');
   app.use(gzippo.staticGzip(__dirname + '/static', {
     maxAge: 86400*365
   }));
-/*
-  app.use(gzippo.staticGzip(__dirname, {
-    contentTypeMatch: /html/
-  }));
-*/
 });
 
 app.configure('development', function(){
-  debug = true;
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+
 app.configure('production', function(){
   app.use(express.errorHandler());
   app.enable('view cache');
@@ -48,35 +43,39 @@ function capitalize(str){
 }
 
 var eventData = {
-  "2011-bangalore": {
+  "bangalore-2011": {
     doAttendId: 4417,
     time: "October 1, 2011",
     city: "Bangalore"
   },
-  "2012-pune": {
+  "pune-2012": {
     doAttendId: 19398,
     time: "January 21, 2012",
     city: "Pune"
+  },
+  "chennai-2012": {
+    doAttendId: 0,
+    time: "February 18, 2012",
+    city: "Chennai"
   }
 };
 
 // Routes
 app.get(routeRegEx, function(req, resp){
   var url = req.url;
-  var params = url.match(/(2011|2012)\-(bangalore|pune|chennai)/);
-  var year = params[1];
-  var city = params[2];
-  var opts = {
+  var params = url.match(/(bangalore|pune|chennai)\-(201[12])/);
+  var year = params[2];
+  var city = params[1];
+  resp.render('main', {
     title: ['JSFoo', capitalize(city), year].join(' '),
     path: [city, year].join('/'),
     eventData: eventData[params[0]]
-  };
-  resp.render('main', opts);
+  });
 });
 
 // Catch all route
 app.use(function(eq, resp){
-  resp.redirect("/2012-pune/");
+  resp.redirect("/pune-2012/");
 });
 
 // prevent server from starting as module - can be used with something like multinode/cluster
@@ -84,8 +83,3 @@ if (!module.parent) {
   app.listen(process.env.app_port || 11728);
   console.info("Started on port %d", app.address().port);
 }
-
-if(!debug){
- require('./ircClient').init(app, debug);
-}
-
